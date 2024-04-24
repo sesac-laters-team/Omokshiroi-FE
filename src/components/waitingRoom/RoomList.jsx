c36a88b197d9f4cb69400932e1fee93de4cd26cb
hyein310 <kanghyein310@naver.com>
2024-04-24 18:46:40 +0900

    feat: 방 만들기 & 방 목록 구현

--- src/components/waitingRoom/RoomList.jsx
+++ src/components/waitingRoom/RoomList.jsx
@@ -1,14 +1,76 @@
 import CreateRoom from "./CreateRoom";
-// import io from "socket.io-client";
-import { useState } from "react";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io.connect("http://localhost:8081", {
+    autoConnect: false,
+});
 
 export default function RoomList() {
+    const initSocketConnect = () => {
+        if (!socket.connected) socket.connect();
+    };
+
+    useEffect(() => {
+        initSocketConnect();
+    }, []);
     // 방 만들기 모달 UI state
     const [createRoom, setCreateRoom] = useState(false);
+    // const outside = useRef();
 
     // 방 목록 불러오기
-    // const [roomList, setRoomList] = useState([]);
+    // const [roomList, setRoomList] = useState([
+    //     {
+    //         roomId: 1,
+    //         title: "나는야 오목왕",
+    //         timer: "sec30",
+    //         roomPw: "1234",
+    //         socketId: "dkddfk"
+    //     },
+    //     {
+    //         roomId: 2,
+    //         title: "왕초보만",
+    //         timer: "sec60",
+    //         roomPw: "",
+    //         roomPw: "dwfeef",
+    //     },
+    // ]);
 
+    const [roomList, setRoomList] = useState([]);
+
+    useEffect(() => {
+        // socket.emit("roomListData", roomList);
+
+        // socket.on("newRoomList", (title) => {
+        //     console.log(`${title} 방 생성 완료`);
+
+        //     const newRoomList = [
+        //         ...roomList,
+        //         {}
+        //     ];
+
+        //     setRoomList(newRoomList);
+
+        // });
+
+        // 새 방 만들기
+        socket.on("newRoomList", (title, timer, roomPw, roomId) => {
+            const newRoomList = [
+                ...roomList,
+                {
+                    title: title,
+                    timer: timer,
+                    roomPw: roomPw,
+                    roomId: roomId,
+                },
+            ];
+
+            console.log(`${title} 방 생성 완료`);
+            console.log(newRoomList);
+
+            setRoomList(newRoomList);
+        });
+    }, [roomList]);
 
     // const RoomInfo = (room) => {
     //     const handleEnterRoom = () => {
@@ -17,25 +79,36 @@ export default function RoomList() {
     //     }
     // }
 
-
-    console.log(createRoom);
     return (
         <div className="RoomList">
             <div className="logoImg">로고</div>
             <section className="ShowRoomList">
-                <div className="ListTitle">  
+                <div className="ListTitle">
                     <div>방 목록</div>
-                    <button onClick={ () => {setCreateRoom(true)} }>방 만들기</button>
-                    {createRoom === true ? <CreateRoom /> : null}
+                    <button
+                        onClick={() => {
+                            setCreateRoom(true);
+                        }}
+                    >
+                        방 만들기
+                    </button>
+                    {createRoom === true ? (
+                        <CreateRoom socket={socket} />
+                    ) : null}
                 </div>
-                <div className="ListRoom">
+                <div className="ListRoom" style={{ backgroundColor: "pink" }}>
                     <ul>
-                        <li>왕초보</li>
-                        <li>오목한판</li>
-                        <li>들어오세용</li>
+                        {roomList.map((room) => {
+                            return (
+                                <li key={room.id}>
+                                    <span>{room.title}</span>
+                                    <button>입장하기</button>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
             </section>
         </div>
-    )
-} 
+    );
+}
